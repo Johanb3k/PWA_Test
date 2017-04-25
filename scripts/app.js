37bdd09a8c6e708f6ad0c748250fa1abc0e3253a
johanb3k <johan.bekasinski@gmail.com>
2017-04-25 16:44:34 +0200

    changement des appels aux ressources

--- scripts/app.js
+++ scripts/app.js
@@ -341,7 +341,7 @@
 
   if ('serviceWorker' in navigator) {
     navigator.serviceWorker
-             .register('./service-worker.js')
+             .register('scripts/service-worker.js')
              .then(function() { console.log('Service Worker Registered'); });
   }
 })();
