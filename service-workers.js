fd52ca4b950b1ec882a4ef53650d9a6b65d935ec
BEKASINSKI Johan <A660474@fr01.awl.atosorigin.net>
2017-04-26 15:29:17 +0200

    Ajout des fichiers manifest + service workers

---
+++ service-workers.js
@@ -0,0 +1,78 @@
+var cacheName = 'weatherPWA-step-6-2';
+var filesToCache = [
+  '/PWA_Test/',
+  '/PWA_Test/index.html',
+  '/PWA_Test/scripts/app.js',
+  '/PWA_Test/styles/inline.css',
+  '/PWA_Test/images/clear.png',
+  '/PWA_Test/images/cloudy-scattered-showers.png',
+  '/PWA_Test/images/cloudy.png',
+  '/PWA_Test/images/fog.png',
+  '/PWA_Test/images/ic_add_white_24px.svg',
+  '/PWA_Test/images/ic_refresh_white_24px.svg',
+  '/PWA_Test/images/partly-cloudy.png',
+  '/PWA_Test/images/rain.png',
+  '/PWA_Test/images/scattered-showers.png',
+  '/PWA_Test/images/sleet.png',
+  '/PWA_Test/images/snow.png',
+  '/PWA_Test/images/thunderstorm.png',
+  '/PWA_Test/images/wind.png'
+];
+
+self.addEventListener('install', function(e) {
+  console.log('[ServiceWorker] Install');
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      console.log('[ServiceWorker] Caching app shell');
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+self.addEventListener('activate', function(e) {
+  console.log('[ServiceWorker] Activate');
+  e.waitUntil(
+    caches.keys().then(function(keyList) {
+      return Promise.all(keyList.map(function(key) {
+        if (key !== cacheName) {
+          console.log('[ServiceWorker] Removing old cache', key);
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
+  return self.clients.claim();
+});
+
+self.addEventListener('fetch', function(e) {
+  console.log('[Service Worker] Fetch', e.request.url);
+  var dataUrl = 'https://query.yahooapis.com/v1/public/yql';
+  if (e.request.url.indexOf(dataUrl) > -1) {
+    /*
+     * When the request URL contains dataUrl, the app is asking for fresh
+     * weather data. In this case, the service worker always goes to the
+     * network and then caches the response. This is called the "Cache then
+     * network" strategy:
+     * https://jakearchibald.com/2014/offline-cookbook/#cache-then-network
+     */
+    e.respondWith(
+      caches.open(cacheName).then(function(cache) {
+        return fetch(e.request).then(function(response){
+          cache.put(e.request.url, response.clone());
+          return response;
+        });
+      })
+    );
+  } else {
+    /*
+     * The app is asking for app shell files. In this scenario the app uses the
+     * "Cache, falling back to the network" offline strategy:
+     * https://jakearchibald.com/2014/offline-cookbook/#cache-falling-back-to-network
+     */
+    e.respondWith(
+      caches.match(e.request).then(function(response) {
+        return response || fetch(e.request);
+      })
+    );
+  }
+});
